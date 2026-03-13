a7163ef6296ecb423ac1baf38081bf2c63a4137d
Abel <abel.romerotavio@gmail.com>
2026-03-13 17:36:12 +0000

    SpendlyGPT con contexto

--- src/IA API/IO.js
+++ src/IA API/IO.js
@@ -7,16 +7,23 @@ const ai = new GoogleGenAI({});
 
 
 async function main() {
-
-    const response = await ai.models.generateContent({
+    const chat = await ai.chats.create({
         model: "gemini-3-flash-preview",
-        contents: "Dime un truco de magia",
         config: {
-            systemInstruction:
-                "Eres un asesor financiero experto para la app 'Spendly' Tu tono es cercano pero profesional Reglas: 1. Solo respondes preguntas sobre ahorro, presupuestos y economía. 2. Si te preguntan algo fuera de finanzas, di que no puedes ayudar con eso. 3. No des consejos de inversión en bolsa o cripto específicos."
-        }
+            systemInstruction:  "Eres un asesor financiero experto para la app 'Spendly' Tu tono es cercano pero profesional Reglas: 1. Solo respondes preguntas sobre ahorro, presupuestos y economía. 2. Si te preguntan algo fuera de finanzas, di que no puedes ayudar con eso. 3. No des consejos de inversión en bolsa o cripto específicos."
+
+        },
+
     });
-    console.log(response.text);
+    var N_mensaje = 0
+    const mensajes = ["recuedame mi objetivo"]
+    const Datos_user = "No respondas a este mensaje, es solo información del usuario para el resto del chat. El usuario Gana 1500 euros. Gasta 600 en el alquiler. 100 en luz y agua. 100 en suscripciones de streaming. 300 en comida. ahorra 100 al mes. gasta el resto en hobbies. Su objetivo es ahorrar 10.000 euros."
+    await chat.sendMessage({message: Datos_user})
+    while (N_mensaje < 1) {
+        var response = await chat.sendMessage({message: mensajes[N_mensaje]})
+        N_mensaje++
+        console.log(response.text)
+    }
 }
 
 main();
